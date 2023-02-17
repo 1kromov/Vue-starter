47a54975eb84114761c9dfd2fa71ce5443fce4ec
Abdulaziz Ikromov <abdulazizikromov17@gmail.com>
2023-02-17 08:46:20 +0500

    starter vue with tailwind

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1 class="bg-green-400 flex justify-center h-10 items-center text-white">Hello</h1>
+  </div>
+
+</template>
+
+
+
+<script>
+export default {
+  name:"App",
+}
+
+
+
+</script>
+
+
+
+<style>
+
+
+
+
+
+</style>
